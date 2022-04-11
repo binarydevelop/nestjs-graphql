a2d284b3c4566d7b73352822ac7c936dadf214cb
Tushar Roy <binarydevelop@gmail.com>
2022-04-11 22:21:47 +0530

    add: Basic folder structure

---
+++ src/pets/pets.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PetsService } from './pets.service';
+import { PetsResolver } from './pets.resolver';
+
+@Module({
+  providers: [PetsService, PetsResolver],
+  controllers: []
+})
+export class PetsModule {}
